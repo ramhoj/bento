75086454b6ce8c82b193294115e971b086ebb29f
Nicklas Ramhöj <ramhoj@gmail.com>
2010-09-15 21:52:31 +0200

    Got to stick with Rails.application.routes.draw for now...

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
-# Bento::Application.routes.draw do
+# Note: In comming versions of Rails, Rails.application
+# should be replaced by Bento::Engine
 Rails.application.routes.draw do
   resources :accounts, :controller => 'bento/accounts', :only => %w[new]
 end
