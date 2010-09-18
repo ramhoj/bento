f2c18acdd327d69be4227720dba5f303e9431162
Nicklas Ramhöj <ramhoj@gmail.com>
2010-09-18 21:46:55 +0200

    Added a create route for the accounts resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 # Note: In comming versions of Rails, Rails.application
 # should be replaced by Bento::Engine
 Rails.application.routes.draw do
-  resources :accounts, :controller => 'bento/accounts', :only => %w[new]
+  resources :accounts, :controller => 'bento/accounts', :only => %w[new create]
 end
