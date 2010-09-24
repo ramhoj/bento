89865c9652da481ed6c0c569124fea4876ffb896
Nicklas Ramhöj <dev+nicklas@elabs.se>
2010-09-24 15:56:50 +0200

    Added form errors helpers to rails app

---
+++ app/helpers/form_errors_helper.rb
@@ -0,0 +1,15 @@
+module FormErrorsHelper
+  def error_message_for(object, message=nil)
+    if object.errors.present?
+      content_tag(:div, :class => 'ErrorMessages') do
+        content_tag(:h3, message || "#{object.class.model_name.human} could not be saved.")
+      end
+    end
+  end
+end
+
+ActionView::Helpers::FormBuilder.class_eval do
+  def error_message(message=nil)
+    @template.error_message_for(@object, message)
+  end
+end
