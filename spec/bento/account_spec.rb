dc47d8d20537a6752d5fc95fe2ba6c31d318af8a
Nicklas Ramhöj <ramhoj@gmail.com>
2010-10-03 13:33:16 +0200

    Forgot to stage the specs for user creation hook.

--- spec/bento/account_spec.rb
+++ spec/bento/account_spec.rb
@@ -51,4 +51,30 @@ describe Bento::Account do
     it { should_not have_user_accessors }
     it { should respond_to(:users) }
   end
+
+  context "user accessors and user association" do
+    let(:account_params) do
+      { :name => "Hashrocket", :first_name => "Obie", :last_name => "Fernandez", :email => "obie@hashrocket.com", :password => "test1234" }
+    end
+    subject { AllUserTestAccount.new }
+    it { should_not be_invalid_without(:name) }
+    it { should have_user_accessors }
+    it { should respond_to(:users) }
+
+    context "successfully creating a user" do
+      subject do
+        account = AllUserTestAccount.create!(account_params)
+        account.users.first
+      end
+
+      its(:email) { should == "obie@hashrocket.com" }
+      its(:first_name) { should == "Obie" }
+      its(:last_name) { should == "Fernandez" }
+    end
+
+    context "unsuccessfully creating a user" do
+      subject { AllUserTestAccount.new(account_params.merge(:email => "")).tap(&:valid?) }
+      it { subject.errors.should include(:email) }
+    end
+  end
 end
