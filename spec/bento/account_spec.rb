4d47405e62a61666e6ac05e78c1c34bb54380466
Nicklas Ramhöj <ramhoj@gmail.com>
2010-10-04 19:51:35 +0200

    Added a trial_days_remaining method to bento model module.

--- spec/bento/account_spec.rb
+++ spec/bento/account_spec.rb
@@ -8,13 +8,43 @@ class UserAccessorsTestAccount < Bento::AccountTest;            bento_account(:u
 class UserAssociationTestAccount < Bento::AccountTest;          bento_account(:user_association);                  end
 class AllUserTestAccount < Bento::AccountTest;                  bento_account(:user_accessors, :user_association); end
 class ServeralOptionsAccessorsTestAccount < Bento::AccountTest; bento_account(:user_accessors, :validations);      end
+class TrialTestAccount < Bento::AccountTest;                    bento_account(:trial);                             end
 
 describe Bento::Account do
+  let(:account_params) do
+    { :name => "Hashrocket", :first_name => "Obie", :last_name => "Fernandez", :email => "obie@hashrocket.com", :password => "test1234" }
+  end
+
   context "all" do
     subject { AllTestAccount.new }
     it { should be_invalid_without(:name) }
     it { should have_user_accessors }
     it { should respond_to(:users) }
+    it { should respond_to(:trial_days_remaining) }
+    it { should respond_to(:build_user) }
+
+    describe ".build_user" do
+      context "successfully creating a user" do
+        subject do
+          account = AllTestAccount.create!(account_params)
+          account.users.first
+        end
+
+        its(:email) { should == "obie@hashrocket.com" }
+        its(:first_name) { should == "Obie" }
+        its(:last_name) { should == "Fernandez" }
+      end
+
+      context "unsuccessfully creating a user" do
+        subject { AllTestAccount.new(account_params.merge(:email => "")).tap(&:valid?) }
+        it { subject.errors.should include(:email) }
+      end
+    end
+
+    describe "#trial_days_remaining" do
+      subject { TrialTestAccount.create!(:name => "Account", :created_at => 6.days.ago).trial_days_remaining }
+      it("returns the number of days that are left in the trial") { should == 24 }
+    end
   end
 
   context "no options" do
@@ -22,6 +52,8 @@ describe Bento::Account do
     it { should be_invalid_without(:name) }
     it { should have_user_accessors }
     it { should respond_to(:users) }
+    it { should respond_to(:trial_days_remaining) }
+    it { should respond_to(:build_user) }
   end
 
   context "validations" do
@@ -29,6 +61,8 @@ describe Bento::Account do
     it { should be_invalid_without(:name) }
     it { should_not have_user_accessors }
     it { should_not respond_to(:users) }
+    it { should_not respond_to(:trial_days_remaining) }
+    it { should_not respond_to(:build_user) }
   end
 
   context "user accessors" do
@@ -36,6 +70,8 @@ describe Bento::Account do
     it { should_not be_invalid_without(:name) }
     it { should have_user_accessors }
     it { should_not respond_to(:users) }
+    it { should_not respond_to(:trial_days_remaining) }
+    it { should_not respond_to(:build_user) }
   end
 
   context "several options" do
@@ -43,6 +79,8 @@ describe Bento::Account do
     it { should be_invalid_without(:name) }
     it { should have_user_accessors }
     it { should_not respond_to(:users) }
+    it { should_not respond_to(:trial_days_remaining) }
+    it { should_not respond_to(:build_user) }
   end
 
   context "user association" do
@@ -50,31 +88,25 @@ describe Bento::Account do
     it { should_not be_invalid_without(:name) }
     it { should_not have_user_accessors }
     it { should respond_to(:users) }
+    it { should_not respond_to(:trial_days_remaining) }
+    it { should_not respond_to(:build_user) }
+  end
+
+  context "trial" do
+    subject { TrialTestAccount.new }
+    it { should_not be_invalid_without(:name) }
+    it { should_not have_user_accessors }
+    it { should_not respond_to(:users) }
+    it { should respond_to(:trial_days_remaining) }
+    it { should_not respond_to(:build_user) }
   end
 
   context "user accessors and user association" do
-    let(:account_params) do
-      { :name => "Hashrocket", :first_name => "Obie", :last_name => "Fernandez", :email => "obie@hashrocket.com", :password => "test1234" }
-    end
     subject { AllUserTestAccount.new }
     it { should_not be_invalid_without(:name) }
     it { should have_user_accessors }
     it { should respond_to(:users) }
-
-    context "successfully creating a user" do
-      subject do
-        account = AllUserTestAccount.create!(account_params)
-        account.users.first
-      end
-
-      its(:email) { should == "obie@hashrocket.com" }
-      its(:first_name) { should == "Obie" }
-      its(:last_name) { should == "Fernandez" }
-    end
-
-    context "unsuccessfully creating a user" do
-      subject { AllUserTestAccount.new(account_params.merge(:email => "")).tap(&:valid?) }
-      it { subject.errors.should include(:email) }
-    end
+    it { should_not respond_to(:trial_days_remaining) }
+    it { should respond_to(:build_user) }
   end
 end
