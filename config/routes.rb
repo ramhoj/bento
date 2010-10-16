a89848f7ab17e11526aa01d18aa3b8abe50d3509
Nicklas Ramhöj <ramhoj@gmail.com>
2010-10-16 15:03:00 +0200

    Made the account creat action more complex to allow for diffrent creation views (sign up and admin creation)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 # Note: In comming versions of Rails, Rails.application
 # should be replaced by Bento::Engine
 Rails.application.routes.draw do
-  resources :accounts, :controller => 'bento/accounts', :only => %w[new create]
+  resources :accounts, :controller => 'bento/accounts' do
+    collection { get :sign_up }
+  end
 end
--- features/step_definitions/session_steps.rb
+++ features/step_definitions/session_steps.rb
@@ -2,3 +2,8 @@ Given /^I am signed in as "([^"]*)"$/ do |email|
   user = User.find_by_email(email)
   sign_in(user)
 end
+
+Given /^I am logged in as admin$/ do
+  user = User.make
+  sign_in(user)
+end
