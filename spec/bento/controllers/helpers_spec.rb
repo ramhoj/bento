8cef0ae1a418f845866a3afc6c6f29ef4e3da99a
Nicklas Ramhöj <dev+ramhoj@elabs.se>
2010-10-29 15:24:43 +0200

    Added spec for current_account

--- spec/bento/controllers/helpers_spec.rb
+++ spec/bento/controllers/helpers_spec.rb
@@ -1,4 +1,17 @@
 require 'spec_helper'
 
+class AccountController < ActionController::Base; end
+
 describe Bento::Controllers::Helpers do
+  subject { AccountController.new }
+
+  describe "#current_account" do
+    let(:account) { Account.make }
+    let(:user) { User.make(:account => account) }
+    before { subject.stubs(:current_user).returns(user) }
+
+    it "returns the current users account" do
+      subject.current_account.should == account
+    end
+  end
 end
