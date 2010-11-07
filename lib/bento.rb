57587b7ef85fcb892f6b6096ab169e4dd3ab7fa1
Nicklas Ramhöj <ramhoj@gmail.com>
2010-11-07 14:29:14 +0100

    Removed specs for unused code.

--- lib/bento.rb
+++ lib/bento.rb
@@ -1,11 +1,6 @@
 module Bento
   require "bento/rails"
   require "bento/models/account"
-
-  def self.configure
-    yield self
-  end
-
   require "bento/controllers/helpers"
   require "bento/controllers/account_scopable"
 end
