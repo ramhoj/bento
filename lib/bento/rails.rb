fe928f214feb94af144dca4ceaf2de9332622f8f
Nicklas Ramhöj <ramhoj@gmail.com>
2010-11-07 10:35:01 +0100

    Trying to figgue out how devise loads it's dynamicly named helpers

--- lib/bento/rails.rb
+++ lib/bento/rails.rb
@@ -1,4 +1,12 @@
+require 'bento/rails/routes'
+
 module Bento
   class Engine < ::Rails::Engine
+    # Force routes to be loaded if we are doing any eager load.
+    config.before_eager_load { |app| app.reload_routes! }
+
+    initializer "bento.url_helpers" do
+      Bento.include_helpers(Bento::Controllers)
+    end
   end
 end
