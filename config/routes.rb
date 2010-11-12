300c03b452dcbf3addf3a918f6b4ee6fa7b37d25
Nicklas Ramhöj <dev+ramhoj@elabs.se>
2010-11-12 16:27:48 +0100

    Routes are now defined by using bento_for :resource_name

--- config/routes.rb
+++
@@ -1,7 +0,0 @@
-# Note: In comming versions of Rails, Rails.application
-# should be replaced by Bento::Engine
-Rails.application.routes.draw do
-  resources :accounts, :controller => 'bento/accounts' do
-    collection { get :sign_up }
-  end
-end
