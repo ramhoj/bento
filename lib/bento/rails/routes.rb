77ef3416f78dc3258244575a42bc406363d1350e
Nicklas Ramhöj <ramhoj@gmail.com>
2010-11-14 18:59:45 +0100

    Routes can now be nested within bento_for.

--- lib/bento/rails/routes.rb
+++ lib/bento/rails/routes.rb
@@ -1,12 +1,13 @@
 module ActionDispatch::Routing
   class Mapper
-    def bento_for(*resource_names)
+    def bento_for(*resource_names, &block)
       resource_names.map!(&:to_sym)
       resource_names.each do |resource_name|
         Bento::Controllers::Helpers.define_helpers(resource_name)
 
         resources(resource_name, :controller => account_controller(resource_name)) do
           collection { get :sign_up }
+          yield if block_given?
         end
       end
     end
