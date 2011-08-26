c95785985416f08222a8422dce7fe2e512aeaef3
Nicklas Ramhöj <dev+ramhoj@elabs.se>
2011-08-26 15:45:16 +0200

    Removed useless comments

--- spec/rails_app/app/models/user.rb
+++ spec/rails_app/app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :validatable
-  # Also available: :token_authenticatable, :confirmable, :lockable,
-  # :timeoutable :registerable, :trackable, :rememberable, :recoverable
 
   attr_accessible :first_name, :last_name, :email, :password, :password_confirmation, :account
 
