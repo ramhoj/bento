a54726da21290082f588dbc8e3e6fae849e96d85
Nicklas Ramhöj <dev+ramhoj@elabs.se>
2011-08-26 16:35:40 +0200

    Actually using database_cleaner is a good idea.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,16 +8,18 @@ require 'rspec/rails'
 require "rails/test_help"
 
 require 'capybara/dsl'
+require 'database_cleaner'
 
 Dir["#{File.expand_path(File.dirname(__FILE__))}/support/*.rb"].each {|f| require f}
 
 Capybara.app = RailsApp::Application
+DatabaseCleaner.strategy = :truncation
 
 RSpec.configure do |config|
   config.mock_with :rspec
   config.include Capybara::DSL
   config.use_transactional_fixtures = true
-  config.before { Account.delete_all }
+  config.before { DatabaseCleaner.clean }
   config.before(:all)  { Sham.reset(:before_all)  }
   config.before(:each) { Sham.reset(:before_each) }
 end
